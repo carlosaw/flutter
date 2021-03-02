83bcb233c4d6640d3187aa8e9d5a2c19e5ca2108
unknown <carlosfreevg@gmail.com>
2021-03-01 21:36:59 -0400

    FirstCommit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MeuApp());
+
+class MeuApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+          appBar: AppBar(title: Text('Meu primeiro App')),
+          body: Center(child: Text('Olá Mundo'))),
+    );
+  }
+}
