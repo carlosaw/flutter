44c1bbb3a13b93999cf8f72fd9cfd94ca0de6033
Your Name <you@example.com>
2021-03-03 10:59:05 -0400

    FirstColumn

--- lib/main.dart
+++ lib/main.dart
@@ -16,52 +16,31 @@ class MeuApp extends StatelessWidget {
                       fontSize: 18,
                     ))),
             body: Column(children: <Widget>[
-              Image(width: 450, image: AssetImage('assets/muro.jpg')),
-              Row(children: [
-                Column(
-                  children: [
-                    Container(),
-                    Row(
-                      children: [
-                        Text('KOTEL',
-                            style: TextStyle(
-                                fontSize: 10, fontWeight: FontWeight.bold)),
-                      ],
-                    ),
-                    Row(
-                      children: [
-                        Text('Jerusalém, Israel',
-                            style: TextStyle(fontSize: 10)),
-                      ],
-                    )
-                  ],
-                ),
-                Column(
-                  children: [
-                    Row(
-                      children: [
-                        Container(
-                            child:
-                                Icon(Icons.star, size: 20, color: Colors.blue)),
-                        Text('3.985', style: TextStyle(fontSize: 10))
-                      ],
-                    )
-                  ],
-                )
-              ]),
-              Row(children: [
-                Expanded(child: Container(height: 100, color: Colors.pink))
-              ]),
-            ])
-            /*body: Column(
-              children: <Widget>[
-                Image(width: 450, image: AssetImage('assets/muro.jpg')),
-                Container(
-                    width: 250,
-                    child:
-                        Image.network('https://www.google.com.br/google.jpg'))
-              ],
-            )*/
-            ));
+              Image(image: AssetImage('assets/muro.jpg')),
+              Container(
+                  padding: EdgeInsets.all(20),
+                  child: Row(
+                    children: [
+                      Expanded(
+                          child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text('KOTEL',
+                              style: TextStyle(
+                                fontSize: 13,
+                                fontWeight: FontWeight.bold,
+                              )),
+                          Text('Jerusalém, Israel',
+                              style:
+                                  TextStyle(fontSize: 12, color: Colors.grey))
+                        ],
+                      )),
+                      Row(children: <Widget>[
+                        Icon(Icons.star, color: Colors.blue),
+                        Text('9.875', style: TextStyle(fontSize: 12))
+                      ])
+                    ],
+                  )),
+            ])));
   }
 }
