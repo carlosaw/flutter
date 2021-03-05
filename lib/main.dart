29dbe94ca63c59c2827690b1bfe766957bfd2675
unknown <carlosfreevg@gmail.com>
2021-03-04 21:41:14 -0400

    Login

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:provider/provider.dart';
 import 'src/models/contador.dart';
 import 'src/home.dart';
 import 'src/help.dart';
+import 'src/login.dart';
 
 void main() => runApp(MultiProvider(
       providers: [
@@ -18,6 +19,7 @@ class MeuApp extends StatelessWidget {
       routes: {
         '/home': (context) => HomePage(),
         '/help': (context) => HelpPage(),
+        '/login': (context) => LoginPage(),
       },
       initialRoute: '/home',
     );
--- lib/src/home.dart
+++ lib/src/home.dart
@@ -1,41 +1,28 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
-import './models/contador.dart';
-import './formulario.dart';
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(centerTitle: true, title: Text('Home Page')),
-      body: Container(
-          child: Column(children: <Widget>[
-        Formulario(),
-        Container(height: 100),
-        TextField(
-          decoration: InputDecoration(labelText: 'Digite seu nome'),
-          onChanged: (text) {
-            Provider.of<Contador>(context).mudarNome(text);
-          },
-        ),
-        Consumer<Contador>(
-            builder: (context, contador, child) =>
-                Text('Meu nome é: ${contador.nome}')),
-        Text('Contagem'),
-        Consumer<Contador>(
-            builder: (context, contador, child) =>
-                Text(contador.valor.toString())),
-        RaisedButton(
-            child: Text('Aumentar'),
-            onPressed: () {
-              Provider.of<Contador>(context, listen: false).aumentar();
-            }),
-        RaisedButton(
-            child: Text('Ir para HELP'),
-            onPressed: () {
-              Navigator.pushNamed(context, '/help');
-            }),
-      ])),
-    );
+        appBar: AppBar(centerTitle: true, title: Text('Home Page')),
+        body: Center(
+          child: Column(
+            children: <Widget>[
+              Container(
+                  height: 200,
+                  child: Center(
+                    child: Text(
+                      'Bem vindo ao App',
+                      style: TextStyle(fontSize: 20),
+                    ),
+                  )),
+              RaisedButton(
+                  child: Text('Fazer Login'),
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/login');
+                  })
+            ],
+          ),
+        ));
   }
 }
